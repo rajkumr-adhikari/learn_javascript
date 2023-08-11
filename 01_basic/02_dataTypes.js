508fdcf86e71c84612ebcbdac0839e0cfb95baa3
rajkumr-adhikari <140999969+rajkumr-adhikari@users.noreply.github.com>
2023-08-11 17:39:51 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all js code as newer version
+
+//alert(3+3) //we are using node js not browser
+
+console.log(3+3)
+
+let name = "rajkumar"
+let age = 18
+let isLoggedIn = true
+
+/*
+number
+bigint
+string = ""
+boolean
+null => standalone value
+undefined
+symbol => uniqueness
+
+object
+*/
+
+console.log(typeof "rajkumar")
+console.log(typeof null)
