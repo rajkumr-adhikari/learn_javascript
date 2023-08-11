cddcfeebc140380e22469d6940019611cce79e3b
rajkumr-adhikari <140999969+rajkumr-adhikari@users.noreply.github.com>
2023-08-11 17:52:24 +0000

    conversion operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -15,7 +15,26 @@
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn)
 
-let score = 33
-let stringNumber = String(score)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let score = 33
+// let stringNumber = String(score)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// let namee = "rajkumar"
+
+// console.log(typeof namee)
+
+// let myName = Number(namee)
+// console.log(typeof myName)
+// console.log(myName)
+
+let isLoggedIn = 1
+let booleanValue = Boolean(isLoggedIn)
+console.log(booleanValue)
+
+/*
+1 true 
+2 false
+"" false
+"raj" true
+*/
