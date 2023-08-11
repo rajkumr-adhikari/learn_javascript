2834afe3553d794b814645ae8e94c5c265bcab4a
rajkumr-adhikari <140999969+rajkumr-adhikari@users.noreply.github.com>
2023-08-11 18:31:55 +0000

    datatype summary

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,10 @@
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+//=== strict check
+//clean code
+
+console.log("2"===2)
+console.log("2"==2)
---
+++ 01_basic/datatypes.summary.js
@@ -0,0 +1,33 @@
+
+//Primitive data types
+//7 types : String, number, boolean, null, undefined, symbol, BigInt
+
+//Non primitives data types/Reference type
+
+// Array , Objects, Functions
+
+// const score = 100
+// const scoreValue = 100.4
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail; //undefined
+// let userEmaill = undefined
+
+// const id = Symbol("123")
+// const anotherId = Symbol("123")
+
+// const bigNumber = 48953734939487n
+
+// const heros = ["saktiman","nagraj","doga"]
+
+// let myObj = {
+//     name : "raj",
+//     age: 22
+// }
+
+
+const myFunction = function(){
+    console.log("hello world")
+}
+console.log(typeof myFunction)
+
