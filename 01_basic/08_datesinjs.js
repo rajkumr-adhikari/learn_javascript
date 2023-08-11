2c38d6b79e2a69ea871d3040112389421a34aecf
rajkumr-adhikari <140999969+rajkumr-adhikari@users.noreply.github.com>
2023-08-11 19:44:08 +0000

    date and time in js

---
+++ 01_basic/08_datesinjs.js
@@ -0,0 +1,8 @@
+//dates
+
+let myDate = new Date()
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleDateString())
+
+let myCreatedDate = new Date("2023-08-12")
+console.log(myCreatedDate.toDateString())
